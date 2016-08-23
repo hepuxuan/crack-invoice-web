11036fd7470cf0b1cf91a882441adcf90f290d23
Puxuan He <puxuanhe@Puxuans-MacBook.local>
2016-08-22 23:54:33 -0500

    fix the stale socket issue

--- src/client/web-socket/index.js
+++ src/client/web-socket/index.js
@@ -1,11 +1,16 @@
 import { getSocketHostUrl, parseWSMessage } from '../../web-socket-utils';
 import * as MESSAGE_TYPE from '../../message-type';
 
+let ws = null;
+function getWebSocket(clientId) {
+    if (!(ws && ws.readyState === WebSocket.OPEN && ws.readyState === WebSocket.CONNECTING)) {
+        ws = new WebSocket(`${getSocketHostUrl()}?clientId=${encodeURIComponent(clientId)}`);
+    }
+    return ws;
+}
 
 export default function createWebSocketConnection(callbacks, clientId) {
-    const webSocket = new WebSocket(`${getSocketHostUrl()}?clientId=${encodeURIComponent(clientId)}`, ['soap', 'xmpp']);
-
-    webSocket.onmessage= function (e) {
+    getWebSocket(clientId).onmessage = function (e) {
         const message = parseWSMessage(e.data);
         switch (message.type) {
             case MESSAGE_TYPE.SEND_INVOICE:
