dfb1f88e1fc9305c3d71a2c77047653070aba10e
Puxuan He <puxuanhe@Puxuans-MacBook.local>
2016-08-24 00:39:46 -0500

    keep the socket alive

--- src/client/web-socket/index.js
+++ src/client/web-socket/index.js
@@ -1,28 +1,28 @@
 import { getSocketHostUrl, parseWSMessage } from '../../web-socket-utils';
 import * as MESSAGE_TYPE from '../../message-type';
 
-let ws = null;
-function getWebSocket(clientId) {
-    if (!(ws && ws.readyState === WebSocket.OPEN && ws.readyState === WebSocket.CONNECTING)) {
-        ws = new WebSocket(`${getSocketHostUrl()}?clientId=${encodeURIComponent(clientId)}`);
-    }
-    return ws;
-}
-
 export default function createWebSocketConnection(callbacks, clientId) {
-    getWebSocket(clientId).onmessage = function (e) {
-        const message = parseWSMessage(e.data);
-        switch (message.type) {
-            case MESSAGE_TYPE.SEND_INVOICE:
-                if (typeof callbacks.onReceiveInvoice === 'function') {
-                    callbacks.onReceiveInvoice(JSON.parse(message.value));
-                }
-                break;
-            case MESSAGE_TYPE.LOGIN_SUCCESS:
-                if (typeof callbacks.onLoginSuccess === 'function') {
-                    callbacks.onLoginSuccess();
-                }
-                break;
-        }
-    };
+    let ws = null;
+
+    (function createWs() {
+        ws = new WebSocket(`${getSocketHostUrl()}?clientId=${encodeURIComponent(clientId)}`);
+        ws.onmessage = (e) => {
+            const message = parseWSMessage(e.data);
+            switch (message.type) {
+                case MESSAGE_TYPE.SEND_INVOICE:
+                    if (typeof callbacks.onReceiveInvoice === 'function') {
+                        callbacks.onReceiveInvoice(JSON.parse(message.value));
+                    }
+                    break;
+                case MESSAGE_TYPE.LOGIN_SUCCESS:
+                    if (typeof callbacks.onLoginSuccess === 'function') {
+                        callbacks.onLoginSuccess();
+                    }
+                    break;
+            }
+        };
+        ws.onclose = () => {
+            createWs();
+        };
+    }());
 }
--- src/server/server.babel.js
+++ src/server/server.babel.js
@@ -4,7 +4,6 @@ const wss = new require('ws').Server({ server: server, path: '/ws' });
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
-const MESSAGE_TYPE = require('../message-type');
 const WebSocketUtils = require('../web-socket-utils');
 
 const mockInvoice = {
@@ -25,29 +24,19 @@ wss.on('connection', (webSocket) => {
     }
 
     webSocket.on('close', function close() {
-        console.log(`closing:${clientId}`);
-        delete webSocketMap[clientId];
+        if (clientId) {
+            console.log(`closing:${clientId}`);
+            delete webSocketMap[clientId]; 
+        }
     });
 
     webSocket.on('message', (_message) => {
         const message = WebSocketUtils.parseWSMessage(_message);
-        const webSocketClient = webSocketMap[message.clientId];
-        switch(message.type) {
-            case MESSAGE_TYPE.LOGIN_SUCCESS:
-                if (webSocketClient) {
-                    webSocketClient.send(WebSocketUtils.createWSMessage(MESSAGE_TYPE.LOGIN_SUCCESS, true));
-                }
-                break;
-            case MESSAGE_TYPE.SEND_INVOICE:
-                if (webSocketClient) {
-                    webSocketClient.send(WebSocketUtils.createWSMessage(MESSAGE_TYPE.SEND_INVOICE, message.value));
-                }
-                break;
+        const webSocketClient = message.clientId && webSocketMap[message.clientId];
+        if (webSocketClient) {
+            webSocketClient.send(WebSocketUtils.createWSMessage(message.type, message.value));
         }
     });
-
-    // webSocket.send(WebSocketUtils.createWSMessage(MESSAGE_TYPE.SEND_INVOICE, JSON.stringify(mockInvoice)));
-    // webSocket.send(WebSocketUtils.createWSMessage(MESSAGE_TYPE.LOGIN_SUCCESS, true));
 });
 
 server.on('request', app);
